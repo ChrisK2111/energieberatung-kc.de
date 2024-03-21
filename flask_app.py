e635346766192421ca309c680109c1c58feb3777
Christian Karczewski <christian.karczewski@icloud.com>
2024-03-21 06:47:33 +0000

    sfp hinzugefügt

--- flask_app.py
+++ flask_app.py
@@ -24,6 +24,13 @@ def init_session():
         session['ids'] = random.randint(0,int(1e10))
     return render_template('index.html')
 
+@app.route('/sfp')
+def sfp():
+    return render_template('sfp/index.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt/index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5001,debug=True)
