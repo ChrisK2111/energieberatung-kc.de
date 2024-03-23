92b7905b2dbc1a3462f7a2f6a6096b8ffa599e62
ChrisK2111 <christian.karczewski@icloud.com>
2024-03-23 06:19:29 +0100

    retrofit maßnahmen aus der eingabemaske werden übernommen

--- py/sfp_app.py
+++ py/sfp_app.py
@@ -1,4 +1,4 @@
-from energy_calculator import building, weather_data
+from .energy_calculator import Building, weather_data
 
 def calc_sfp(sfp_form):
     yr = int(sfp_form['floatingInput_ConstructionYear']);
@@ -15,8 +15,97 @@ def calc_sfp(sfp_form):
         ug_floor_count = 1;
     else:
         ug_floor_count = 0;
-
-
+    
     building = Building(yr,a_nra,floor_count,ug_floor_count,ext_wall_count)
+    initial_rf_measures = get_completed_retrofit_measures(sfp_form)
+
+    for rf in initial_rf_measures:
+        building.retrofit_component(rf['type'],rf['component'], rf['year'])
+    
     q_net = building.calc_energy_demand_yearly(weather_data)
+
     return q_net
+
+def get_completed_retrofit_measures(sfp_form):
+    year_of_constr  = float(sfp_form['floatingInput_ConstructionYear'])
+    rf_ext_walls    = float(sfp_form['selection-extWalls'])
+    rf_windows      = float(sfp_form['selection-windows'])
+    rf_heat_sys     = float(sfp_form['selection-heatingSystem'])
+    rf_roof         = float(sfp_form['selection-roof'])
+    rf_basement     = sfp_form.get('check-basementInsulation', False)
+
+    rf = []
+
+    if rf_ext_walls == 2:
+        rf_year = max(year_of_constr + 10, 1996)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'extWalls',
+            'description': 'Dämmung der Außenwände'
+        })
+
+    if rf_ext_walls == 3:
+        rf_year = max(year_of_constr + 20, 2010)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'extWalls',
+            'description': 'Dämmung der Außenwände'
+        })
+
+    if rf_windows == 2:
+        rf_year = max(year_of_constr + 10, 1996)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'windows',
+            'description': 'Fenstertausch'
+        })
+
+    if rf_windows == 3:
+        rf_year = max(year_of_constr + 20, 2010)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'windows',
+            'description': 'Fenstertausch'
+        })
+
+    if rf_roof == 2:
+        rf_year = max(year_of_constr + 30, 2010)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'roof',
+            'description': 'Dachdämmung und Austausch der Dachflächenfenster'
+        })
+
+    if rf_heat_sys == 2:
+        rf_year = max(year_of_constr + 20, 1996)
+        rf.append({
+            'year': rf_year,
+            'type': 'heating',
+            'component': 'gas-70',
+            'description': 'Einbau eines effizienten Heizkessels'
+        })
+
+    if rf_heat_sys == 3:
+        rf_year = max(year_of_constr + 30, 2010)
+        rf.append({
+            'year': rf_year,
+            'type': 'heating',
+            'component': 'gas-55',
+            'description': 'Einbau eines effizienten Heizkessels'
+        })
+
+    if rf_basement:
+        rf_year = max(year_of_constr + 30, 2010)
+        rf.append({
+            'year': rf_year,
+            'type': 'envelope',
+            'component': 'basement',
+            'description': 'Dämmung der Kellerdecke'
+        })
+
+    return rf
